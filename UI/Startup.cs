f1ebac80b04c9810dab564c1cdb5238b776c350b
Arthur BROUART <84630114+ArturitoGit@users.noreply.github.com>
2021-11-21 14:03:58 +0100

    Rename ui project

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace FileServer
+namespace UI
 {
     public class Startup
     {
